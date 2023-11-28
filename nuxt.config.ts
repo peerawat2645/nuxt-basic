f584ae88f45635f6b59dc92ac6fff25762d5eb27
Peerawat Wongmek <78299267+peerawat2645@users.noreply.github.com>
2023-11-28 09:41:23 +0700

    initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
+    head: {
+      title:'demo-basic',
+      meta:[
+        {
+          name:"description",
+          content:"รายละเอียดเว็บไซต์"
+        }
+      ]
+    }
+  }
+
